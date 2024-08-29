899ff2bab89223176a82ea67296fbac6fcb351b4
zenitugo <ugochiukaegbu19@gmail.com>
2024-08-29 16:54:57 +0100

    eks creation script

--- terraform-eks/main.tf
+++ terraform-eks/main.tf
@@ -294,14 +294,14 @@ resource "aws_iam_role_policy_attachment" "ContainerRegistry" {
 
 
 # IAM role for csi drive addon
-resource "aws_iam_role" "eks_ebs_csi_driver" {
+resource "aws_iam_role" "ebs_csi_driver" {
   assume_role_policy              = data.aws_iam_policy_document.csi.json
-  name                            = var.role_name
+  name                            = var.csi_role_name
 }
 
 #Policy attachemet to iam role addon
 resource "aws_iam_role_policy_attachment" "amazon_ebs_csi_driver" {
-  role                            = aws_iam_role.eks_ebs_csi_driver.name
+  role                            = aws_iam_role.ebs_csi_driver.name
   policy_arn                      = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
 
@@ -315,8 +315,8 @@ resource "aws_iam_role" "load_balancer_controller" {
 }
 
 # Create a policy to the iam role of the load balancer controller
-resource "aws_iam_policy" "aws_load_balancer_controller" {
-  policy                          = file("${path.module}/controller.json")
+resource "aws_iam_policy" "controller_policy" {
+  policy                          = jsonencode(jsondecode(file("controller.json")))
   name                            = var.controller_policyname
 }
 
@@ -324,7 +324,7 @@ resource "aws_iam_policy" "aws_load_balancer_controller" {
 # Attach a policy to the the role of the load balancer
 resource "aws_iam_role_policy_attachment" "attach_policy" {
   role                            = aws_iam_role.load_balancer_controller.name
-  policy_arn                      = aws_iam_policy.aws_load_balancer_controller.arn
+  policy_arn                      = aws_iam_policy.controller_policy.arn
 }
 
 
@@ -334,13 +334,13 @@ resource "aws_iam_role_policy_attachment" "attach_policy" {
 
 resource "aws_eks_cluster" "eks-cluster" {
   name                    = var.cluster
-  role_arn                = var.eks-clusterrole
+  role_arn                = aws_iam_role.eksrole.arn
   
   
 
   vpc_config {
     # private subnets to configure kubernetes nodes and public to expose services from kubernetes to the internet by using load balancer
-    subnet_ids             = concat(var.private-subnet-ids, var.public-subnet-ids) 
+    subnet_ids             = concat(aws_subnet.private-subnet.*.id, aws_subnet.public-subnet.*.id) 
   }
  
 }
@@ -351,13 +351,13 @@ resource "aws_eks_cluster" "eks-cluster" {
 resource "aws_eks_node_group" "ec2-node-group" {
   cluster_name             = aws_eks_cluster.eks-cluster.name
   node_group_name          = var.nodegroup
-  node_role_arn            = var.noderole
+  node_role_arn            = aws_iam_role.worker-node-role.arn
   subnet_ids               = aws_subnet.private-subnet.*.id 
   
 
   scaling_config {
-    desired_size           = 3
-    max_size               = 5
+    desired_size           = 2
+    max_size               = 4
     min_size               = 1
   }
 
@@ -382,7 +382,7 @@ resource "aws_eks_node_group" "ec2-node-group" {
 resource "aws_iam_openid_connect_provider" "eks" {
   client_id_list           = ["sts.amazonaws.com"]
   thumbprint_list          = [data.tls_certificate.eks.certificates[0].sha1_fingerprint]
-  url                      = aws_eks_cluster.eks-cluster.identity[0].oidc[0].issuer
+  url                      = data.tls_certificate.eks.url
 
 }
 
@@ -398,6 +398,6 @@ resource "aws_eks_addon" "csi_driver" {
   cluster_name             = aws_eks_cluster.eks-cluster.name
   addon_name               = var.addon_name
   addon_version            = data.aws_eks_addon_version.latest.version
-  service_account_role_arn = var.ebs-csi-role 
+  service_account_role_arn = aws_iam_role.ebs_csi_driver.arn
 
 }

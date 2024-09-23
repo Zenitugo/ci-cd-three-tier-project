e06d17925ceab7f01b0d49061b2d72bdc4bd8b12
zenitugo <ugochiukaegbu19@gmail.com>
2024-09-23 10:02:14 +0100

    included the domain name to backend CORS

--- api/main.py
+++ api/main.py
@@ -13,12 +13,13 @@ app = FastAPI()
 
 # Allowing CORS for local testing
 origins = [
-    "http://localhost:3000"
+    "http://localhost:3000,https://qr.sapphireaura.com.ng"
 ]
 
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
